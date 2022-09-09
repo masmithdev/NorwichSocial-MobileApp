6c6a18e57c327cff23657a917cf6fecd7f454791
Michael <michael@masmithdev.com>
2022-09-09 20:13:31 +0100

    Update babel config and tsconfig

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [['@babel/plugin-proposal-class-properties']],
+  assumptions: {
+    setPublicClassFields: false,
+  },
+  overrides: [
+    {
+      test: fileName => !fileName.includes('node_modules'),
+      plugins: [
+        [require('@babel/plugin-proposal-class-properties'), { loose: false }],
+      ],
+    },
+  ],
 };
