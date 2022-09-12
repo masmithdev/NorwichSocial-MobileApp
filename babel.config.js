5458b2fb0ec1f5ff46157e1a449a71637e7490cd
Michael <michael@masmithdev.com>
2022-09-12 16:30:05 +0100

    Use correct babel setting

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [['@babel/plugin-proposal-class-properties']],
-  assumptions: {
-    setPublicClassFields: false,
-  },
-  overrides: [
-    {
-      test: fileName => !fileName.includes('node_modules'),
-      plugins: [
-        [require('@babel/plugin-proposal-class-properties'), { loose: false }],
-      ],
-    },
-  ],
+  plugins: [['@babel/plugin-proposal-class-properties', { loose: false }]],
 };
